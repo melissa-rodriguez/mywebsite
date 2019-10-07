a633b8568d34d00c6aa4f8745cad1a0df2becc2a
melissa-rodriguez <48267097+melissa-rodriguez@users.noreply.github.com>
2019-10-07 16:10:04 -0400

    Add files via upload

---
+++ 01_werk-in-progress/werk-in-progress.js
@@ -0,0 +1,13 @@
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw(){
+    background(0);
+   
+    textFont("Muli");
+    fill(255);
+    textSize(70);
+    textAlign(CENTER, CENTER);
+    text("PROG(WERK)RESS", width/2, height/2);
+}
